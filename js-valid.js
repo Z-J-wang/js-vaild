8f4183318bdabfa59f162f3b46cbba73085ecb4a
Jay <zhi-jie.wang@mail.foxconn.com>
2021-02-01 16:11:20 +0800

    feat: 添加批量校验函数

--- js-valid.js
+++ js-valid.js
@@ -26,6 +26,7 @@ function initValid() {
 	formElem.forEach(function (form) {
 		const rules = JSON.parse(form.getAttribute('rules')); // 获取当前 form 的 rules
 		const props = Object.keys(rules); // 从 rules 中提取 prop
+		let formName = form.name; // 表单的 name
 
 		// 遍历 props, 通过 prop 查找表单控件
 		props.forEach((prop) => {
@@ -44,7 +45,8 @@ function initValid() {
 			propRules.forEach((rule) => {
 				// propControl 添加事件监听器
 				addMoreEvents(propControl, prop, rule.trigger, function (e) {
-					validate(propControl, rule, prop);
+					const validate = new Validate(propControl, rule, prop, formName);
+					validate.validate();
 				});
 			});
 		});
@@ -78,56 +80,182 @@ function addMoreEvents(tragetDom, prop, events, callback) {
 	});
 }
 
-/**
- * 表单验证
- * @param {DOM} tragetDom 要验证的元素
- * @param {Object} rule 用户填写的 rules 数组中以一条 rule
- * @param {*} prop 验证的 prop
- */
-function validate(tragetDom, rule, prop) {
-	const validate = new Validate(tragetDom, rule, prop);
-	// 提取 rule 对象中的全部属性逐一遍历
-	Object.keys(rule).forEach((key) => {
-		// 借助 switch 语句对不同的 key 进行不同的处理
-		switch (key.trim()) {
-			case 'message':
-			case 'trigger':
-				// message、trigger 不需要进行处理，直接忽略
-				break;
-			case 'required': {
-				validate.required();
-				break;
-			}
-			case 'min': {
-				validate.min();
-				break;
-			}
-			case 'max': {
-				validate.max();
-				break;
-			}
-			default:
-				//  既不是默认的表单验证关键字，又不是message、trigger，则直接抛错 "未定义"
-				console.error(`${prop} rule 的 ${key} 未定义`);
-				break;
-		}
+function validateAll(formName) {
+	return new Promise((resolve, reject) => {
+		const formElem = document.querySelector(`form[name=${formName}]`); // 获取要进行验证的表单
+		const arr_propsElem = formElem.querySelectorAll(`*[prop]`); // 获取当前表单中全部的 prop 元素
+		const obj_Rules = JSON.parse(formElem.getAttribute('rules')); // 获取当前 form 的 rules
+
+		// 对 arr_propsElem 进行遍历，逐一验证
+		arr_propsElem.forEach(function (propElem) {
+			const propControl = propElem.querySelector('input, select, areatext'); // 获取 prop 元素中的表单元素，即需要验证的元素
+			let prop = propElem.getAttribute('prop'); // 获取 propElem 元素的的 prop 属性值
+
+			// 根据 prop 属性值从 rules 中提取对应的验证规则数组，然后根据数组中的验证规则逐条对需要验证的元素进行验证
+			obj_Rules[prop].forEach((rule) => {
+				const validate = new Validate(propControl, rule, prop, formName);
+				validate.validate();
+			});
+		});
+
+		setTimeout(() => {
+			const arr_Tip = formElem.querySelectorAll('.valid-tip');
+			resolve(arr_Tip.length <= 0);
+		}, 50);
 	});
 }
 
 /**
- * 表单验证规则对象创建
- * @param {*} tragetDom 验证元素
- * @param {*} rule 用户设置的验证规则
- * @param {*} prop prop 属性名
+ * 表单验证对象创建(用于具体表单控件的验证)
+ * 这里采用了构造函数模式和原型模式来创建对象
+ * @param {DOM} tragetDom 要验证的元素
+ * @param {Object} rule 用户填写的 rules 数组中以一条 rule
+ * @param {String} prop 验证的 prop
+ * @param {String} formName 表单 name 属性
  */
-function Validate(tragetDom, rule, prop) {
+function Validate(tragetDom, rule, prop, formName) {
 	this.tragetDom = tragetDom;
 	this.rule = rule;
 	this.prop = prop;
+	this.formElem = document.querySelector(`form[name=${formName}]`);
 }
 
 Validate.prototype = {
 	constructor: Validate,
+
+	/**
+	 * 表单验证
+	 */
+	validate: function () {
+		// 提取 rule 对象中的全部属性逐一遍历
+		Object.keys(this.rule).forEach((key) => {
+			// 借助 switch 语句对不同的 key 进行不同的处理
+			switch (key.trim()) {
+				case 'message':
+				case 'trigger':
+					// message、trigger 不需要进行处理，直接忽略
+					break;
+				case 'required': {
+					this.required();
+					break;
+				}
+				case 'min': {
+					this.min();
+					break;
+				}
+				case 'max': {
+					this.max();
+					break;
+				}
+				default:
+					//  既不是默认的表单验证关键字，又不是message、trigger，则直接抛错 "未定义"
+					console.error(`${this.prop} rule 的 ${key} 未定义`);
+					break;
+			}
+		});
+	},
+	/**
+	 * 提示语处理
+	 * @param {*} ret 验证结果
+	 * @param {*} msg 提示语文本
+	 * @param {*} ruleName 当前验证规则名
+	 */
+	dealWithTip: function (ret, msg, ruleName) {
+		if (!ret) {
+			// 验证失败
+			console.warn(`${this.prop}：${msg}`);
+			this.addTipLabel(msg, ruleName);
+		} else {
+			// 验证成功
+			this.removeTipLabel(ruleName);
+		}
+	},
+
+	/**
+	 * 以携带 prop 属性的元素为参照点，将错误提示作为其最后的子元素添加到页面上
+	 * @param {String} msg 提示语
+	 * @param {String} ruleName 当前验证规则的名字
+	 */
+	addTipLabel: function (msg, ruleName) {
+		// 获取 prop 元素内部已存在的提示语元素
+		let validTipElem = this.formElem.querySelector(
+			`*[prop=${this.prop}] .valid-tip`
+		);
+		let tipAnchor = this.formElem.querySelector(
+			`*[prop=${this.prop}] .tip-anchor`
+		);
+		let tipELem = document.createElement('lable');
+
+		// 如果已存在提示语，则移除
+		if (validTipElem) {
+			validTipElem.remove();
+		}
+
+		// 提示语元素初始化
+		tipELem.style.display = 'block';
+		tipELem.style.color = 'red';
+		tipELem.classList.add('valid-tip');
+		tipELem.setAttribute('rule', ruleName); // rule 属性用于唯一性判断
+		tipELem.innerHTML = msg;
+
+		// 判断 prop 是否存在提示语锚点
+		if (tipAnchor) {
+			// 存在，则将提示语元素插入到锚点中
+			tipAnchor.append(tipELem);
+		} else {
+			// 不存在，则将提示语元素插入到 prop 元素中
+			this.formElem.querySelector(`*[prop=${this.prop}]`).append(tipELem);
+		}
+	},
+
+	/**
+	 * 以携带 prop 属性的元素为参照点，移除提示语元素
+	 * @param {*} ruleName 当前验证规则的名字
+	 */
+	removeTipLabel: function (ruleName) {
+		// 获取 prop 元素内部已存在的提示语元素
+		let validTipElem = this.formElem.querySelector(
+			`*[prop=${this.prop}] .valid-tip`
+		);
+
+		if (validTipElem) {
+			let attr_rule = validTipElem.getAttribute('rule');
+			if (attr_rule == ruleName) {
+				// 如果，已存在的提示语元素的 rule 属性值和当前验证规则的名字一致，则删除提示语元素
+				validTipElem.remove();
+			}
+		}
+	},
+
+	/**
+	 * 用于判断是否已存在提示语。如果已存在提示语，则只允许对已存在的提示语的对应校验规则进行校验。
+	 * 这样是为了防止，其他验证规则覆盖的其他验证规则的提示语
+	 * @param {String} ruleName 当前验证规则的名字
+	 * @returns {Boolean} 是否允许进一步的表单验证
+	 */
+	hasValidTip: function (ruleName) {
+		let ret = false; // return 结果
+		let validTipElem = this.formElem.querySelector(
+			`*[prop=${this.prop}] .valid-tip`
+		); // 获取 prop 元素内部已存在的提示语元素
+
+		if (validTipElem) {
+			// 已存在提示语，则判断是否在当前验证规则的提示语
+			let attr_rule = validTipElem.getAttribute('rule');
+			if (attr_rule != ruleName) {
+				// 不是当前验证规则的提示语，停止验证
+				ret = false;
+			} else {
+				// 是当前验证的提示语，进入具体的验证
+				ret = true;
+			}
+		} else {
+			// 不存在，直接放回 true 进行进一步的验证
+			ret = true;
+		}
+
+		return ret;
+	},
 
 	// 必填
 	required: function () {
@@ -165,107 +293,6 @@ Validate.prototype = {
 			return false;
 		}
 
-		this.dealWithTip(this.prop, val <= this.rule.max, this.rule.message, 'max');
-	},
-
-	/**
-	 * 提示语处理
-	 * @param {*} ret 验证结果
-	 * @param {*} msg 提示语文本
-	 * @param {*} ruleName 当前验证规则名
-	 */
-	dealWithTip: function (ret, msg, ruleName) {
-		if (!ret) {
-			// 验证失败
-			console.warn(`${this.prop}：${msg}`);
-			this.addTipLabel(msg, ruleName);
-		} else {
-			// 验证成功
-			this.removeTipLabel(ruleName);
-		}
-	},
-
-	/**
-	 * 以携带 prop 属性的元素为参照点，将错误提示作为其最后的子元素添加到页面上
-	 * @param {String} msg 提示语
-	 * @param {String} ruleName 当前验证规则的名字
-	 */
-	addTipLabel: function (msg, ruleName) {
-		// 获取 prop 元素内部已存在的提示语元素
-		let validTipElem = document.querySelector(
-			`*[prop=${this.prop}] .valid-tip`
-		);
-		let tipAnchor = document.querySelector(`*[prop=${this.prop}] .tip-anchor`);
-		let tipELem = document.createElement('lable');
-
-		// 如果已存在提示语，则移除
-		if (validTipElem) {
-			validTipElem.remove();
-		}
-
-		// 提示语元素初始化
-		tipELem.style.display = 'block';
-		tipELem.style.color = 'red';
-		tipELem.classList.add('valid-tip');
-		tipELem.setAttribute('rule', ruleName); // rule 属性用于唯一性判断
-		tipELem.innerHTML = msg;
-
-		// 判断 prop 是否存在提示语锚点
-		if (tipAnchor) {
-			// 存在，则将提示语元素插入到锚点中
-			tipAnchor.append(tipELem);
-		} else {
-			// 不存在，则将提示语元素插入到 prop 元素中
-			document.querySelector(`*[prop=${this.prop}]`).append(tipELem);
-		}
-	},
-
-	/**
-	 * 以携带 prop 属性的元素为参照点，移除提示语元素
-	 * @param {*} ruleName 当前验证规则的名字
-	 */
-	removeTipLabel: function (ruleName) {
-		// 获取 prop 元素内部已存在的提示语元素
-		let validTipElem = document.querySelector(
-			`*[prop=${this.prop}] .valid-tip`
-		);
-
-		if (validTipElem) {
-			let attr_rule = validTipElem.getAttribute('rule');
-			if (attr_rule == ruleName) {
-				// 如果，已存在的提示语元素的 rule 属性值和当前验证规则的名字一致，则删除提示语元素
-				validTipElem.remove();
-			}
-		}
-	},
-
-	/**
-	 * 用于判断是否已存在提示语。如果已存在提示语，则只允许对已存在的提示语的对应校验规则进行校验。
-	 * 这样是为了防止，其他验证规则覆盖的其他验证规则的提示语
-	 * @param {String} ruleName 当前验证规则的名字
-	 * @returns {Boolean} 是否允许进一步的表单验证
-	 */
-	hasValidTip: function (ruleName) {
-		let ret = false; // return 结果
-		let validTipElem = document.querySelector(
-			`*[prop=${this.prop}] .valid-tip`
-		); // 获取 prop 元素内部已存在的提示语元素
-
-		if (validTipElem) {
-			// 已存在提示语，则判断是否在当前验证规则的提示语
-			let attr_rule = validTipElem.getAttribute('rule');
-			if (attr_rule != ruleName) {
-				// 不是当前验证规则的提示语，停止验证
-				ret = false;
-			} else {
-				// 是当前验证的提示语，进入具体的验证
-				ret = true;
-			}
-		} else {
-			// 不存在，直接放回 true 进行进一步的验证
-			ret = true;
-		}
-
-		return ret;
+		this.dealWithTip(val <= this.rule.max, this.rule.message, 'max');
 	}
 };
